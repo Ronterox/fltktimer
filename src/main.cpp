a627cbe1d3a3b78cf4b204d1c43a675c995faddd
Ricardo <ricarditomontserrat@gmail.com>
2024-09-03 05:38:46 +0200

    Practicing with a functional input

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,38 @@
+#include <FL/Enumerations.H>
 #include <FL/Fl.H>
 #include <FL/Fl_Box.H>
+#include <FL/Fl_Input.H>
 #include <FL/Fl_Window.H>
 #include <iostream>
 
-#define LOG(x) std::cout << __LINE__ << x << std::endl
+#define LOG(x) std::cout << __FILE__ << ":" << __LINE__ << " " << x << std::endl
 
-int main(int argc, char **argv) {
-	Fl_Window *window = new Fl_Window(340, 180);
-	Fl_Box *box = new Fl_Box(20, 40, 300, 100, "Blocker!");
-	box->box(FL_UP_BOX);
-	box->labelfont(FL_BOLD + FL_ITALIC);
-	box->labelsize(36);
-	box->labeltype(FL_SHADOW_LABEL);
+int main(const int argc, char **argv) {
+	LOG("Setting up widgets...");
+
+	Fl_Window *window = new Fl_Window(640, 360);
+	Fl_Input *input = new Fl_Input(10, 10, 600, 30);
+	Fl_Box *box = new Fl_Box(10, 50, 600, 300, "Hi mom!");
+	box->labelsize(32);
+
+	LOG("Setting up events...");
+	input->when(FL_WHEN_CHANGED);
+	input->callback(
+		[](Fl_Widget *w, void *data) {
+			Fl_Input *input = (Fl_Input *)w;
+			Fl_Box *box = (Fl_Box *)data;
+
+			box->label(input->value());
+		},
+		box);
+
+	LOG("Loading widgets...");
+	window->begin();
+	input->show();
+	box->show();
 	window->end();
+
+	LOG("Loading window...");
 	window->show(argc, argv);
-
-	LOG("hi mom!");
-
 	return Fl::run();
 }
